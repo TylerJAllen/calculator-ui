77e04a701f244b4990db5fcdbc7a732edf61037e
Rose Sponseller and Tyler Allen <rose@>
2017-04-27 16:31:45 -0700

    Add styles to customize the calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// alert(add(number1, number2));
-
 // Business (or back-end) logic:
 
 var add = function(number1, number2) {
@@ -25,43 +16,39 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
+var modulo = function(number1, number2) {
+  return number1 % number2;
+};
+
 // User interface (or front-end) logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event){
+  $("form#calc").submit(function(event){
   event.preventDefault();
-  //Add
+  //Calculate!!!!
     var number1 = parseInt($("#input1").val());
     var number2 = parseInt($("#input2").val());
-    var result = add(number1, number2);
+    var operator = $("input:radio[name=operator]:checked").val();
+    var sum = add(number1, number2);
+    var difference = subtract(number1, number2);
+    var product = multiply(number1, number2);
+    var quotient = divide(number1, number2);
+    var remainder = modulo(number1, number2);
 
-    $("#output").text(result);
-  });
-  //subtract
-  $("form#subtract").submit(function(event){
-  event.preventDefault();
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-    var result = subtract(number1, number2);
-
-    $("#output").text(result);
-  });
-  //multiply
-  $("form#multiply").submit(function(event){
-  event.preventDefault();
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-    var result = multiply(number1, number2);
-
-    $("#output").text(result);
-  });
-  //divide
-  $("form#divide").submit(function(event){
-  event.preventDefault();
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = divide(number1, number2);
-
-    $("#output").text(result);
+    if (operator === "add") {
+      $("#output").text(sum);
+    }
+    else if (operator === "subtract"){
+      $("#output").text(difference);
+    }
+    else if (operator === "multiply"){
+      $("#output").text(product);
+    }
+    else if (operator === "divide"){
+      $("#output").text(quotient);
+    }
+    else if (operator === "modulo"){
+      $("#output").text(remainder);
+    }
   });
 });
