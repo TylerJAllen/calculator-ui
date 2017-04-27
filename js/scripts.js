2e5c154f4e94b5f4fed183d355de2e24ea6a0aa3
Rose Sponseller and Tyler Allen <tyler@>
2017-04-27 15:45:01 -0700

    Add initial files

--- js/scripts.js
+++ js/scripts.js
@@ -31,8 +31,8 @@ $(document).ready(function() {
   $("form#add").submit(function(event){
   event.preventDefault();
   //Add
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
     var result = add(number1, number2);
 
     $("#output").text(result);
